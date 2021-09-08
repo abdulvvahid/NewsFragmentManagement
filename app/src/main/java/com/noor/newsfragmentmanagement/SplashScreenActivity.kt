c2c9942ec181eef373b92b0329711e646a63ba25
Abdul Wahid Noor <abdulwahidnoor@outlook.com>
2021-09-09 00:05:49 +0300

    Create splash screen

---
+++ app/src/main/java/com/noor/newsfragmentmanagement/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.noor.newsfragmentmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import java.util.*
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+                finish()
+            }
+        }, 1000)
+
+    }
+}
